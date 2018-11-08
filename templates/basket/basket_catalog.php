e4cfa32ed95e122485df707344c7766c1f0b0fae
Natalie <snataly108@gmail.com>
2018-11-08 12:55:38 +0300

    добавили Views

---
+++ templates/basket/basket_catalog.php
@@ -0,0 +1,16 @@
+﻿  {% for item in content_data.catalog %}
+            <div class="product"><a href="{{ domain }}good/{{ item.id_good }}/">
+                <img src="/{{ item.foto }}">
+                <div class="product_descript">
+<div class="naming"><h1>{{ item.name }}</h1></div>
+<div class="short_description">{{ item.short_description }} </div>
+</a>
+<a href="javascript:dell_basket_goods('#{{ item.id_good }}')">Удалить товар</a>				
+</div>
+            </div>
+            
+	{% endfor %}
+
+	<form method="post" action="{{ domain }}order/">
+    <input type="submit" name="basket_order" value="Оформить заказ">
+	</form>
---
+++ templates/basket/index.php
@@ -0,0 +1,39 @@
+
+{% include 'header.html' %}
+    
+    <!-- Основной блок -->
+    <div class="main">
+    
+    <!-- Левый блок -->
+    <div class="left">
+        
+        <!-- Меню -->
+	{% include 'menu.html' %}
+        
+        <div class="open">
+            <p>now<br>is<br>open!</p>
+        </div>
+        
+    </div>
+        
+    <!-- Правый блок -->
+    <div class="right">
+	<div class="tovar_catalog">
+ 	{% include 'basket/basket_catalog.php' %}
+	</div>
+		
+    <!-- Нижняя часть главного блока -->
+	{% include 'brand.html' %}
+        
+	{% include 'instagram.html' %}
+    
+     {% include 'network.html' %}
+    </div>
+    
+     
+    
+    </div>
+	{% include 'footer.html' %} 
+</body>
+    
+</html>
